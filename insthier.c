9818916a6e30fb59e6b3d19cc7e17c476e1f489b
m0 <m0@localhost>
2009-02-14 11:07:39 +0000

    insthier regen.

--- insthier.c
+++ insthier.c
@@ -2,6 +2,27 @@
 #include "install.h"
 
 struct install_item insthier[] = {
-  {INST_COPY, "generic-conf.c", 0, ctxt_repos, 0, 0, 0644},
+  {INST_MKDIR, 0, 0, ctxt_bindir, 0, 0, 0755},
+  {INST_MKDIR, 0, 0, ctxt_incdir, 0, 0, 0755},
+  {INST_MKDIR, 0, 0, ctxt_dlibdir, 0, 0, 0755},
+  {INST_MKDIR, 0, 0, ctxt_slibdir, 0, 0, 0755},
+  {INST_MKDIR, 0, 0, ctxt_repos, 0, 0, 0755},
+  {INST_COPY, "c_string-conf.c", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.ads", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.ads", 0, ctxt_incdir, 0, 0, 0644},
+  {INST_COPY, "c_string.ads", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string.ads", 0, ctxt_incdir, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.adb", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.adb", 0, ctxt_incdir, 0, 0, 0644},
+  {INST_COPY, "c_string.adb", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string.adb", 0, ctxt_incdir, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.ali", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string-arrays.ali", 0, ctxt_incdir, 0, 0, 0444},
+  {INST_COPY, "c_string.ali", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string.ali", 0, ctxt_incdir, 0, 0, 0444},
+  {INST_COPY, "c_string.sld", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "c_string.a", "libc_string.a", ctxt_slibdir, 0, 0, 0644},
+  {INST_COPY, "c_string-conf.ld", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY_EXEC, "c_string-conf", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
