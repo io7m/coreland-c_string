fe3ebea7eea39f31b8ca20f93b1d791164ff60a5
m0 <m0@viper.internal.network>
2009-11-16 06:48:59 +0000

    Add terminated versions of all calls

--- c_string-arrays.ads
+++ c_string-arrays.ads
@@ -9,34 +9,57 @@ package C_String.Arrays is
   type String_Array_t is array (Natural range <>) of
     Ada.Strings.Unbounded.Unbounded_String;
 
+  type Allocated_Pointer_Array_t is new Pointer_Array_t;
+
   --
-  -- find NULL
+  -- Find NULL
   --
 
   function Size_Terminated
-    (Pointer : Pointer_Array_t) return Natural;
+    (Pointer : in Pointer_Array_t) return Natural;
+
+  function Size_Terminated
+    (Pointer : in Allocated_Pointer_Array_t) return Natural;
 
   --
   -- array_ptr [Index]
   --
 
   function Index
-    (Pointer : Pointer_Array_t;
-     Size    : Natural;
-     Index   : Natural) return String;
+    (Pointer : in Pointer_Array_t;
+     Size    : in Natural;
+     Index   : in Natural) return String;
 
   function Index
-    (Pointer : Pointer_Array_t;
-     Size    : Natural;
-     Index   : Natural) return Ada.Strings.Unbounded.Unbounded_String;
+    (Pointer : in Pointer_Array_t;
+     Size    : in Natural;
+     Index   : in Natural) return Ada.Strings.Unbounded.Unbounded_String;
 
   function Index_Terminated
-    (Pointer : Pointer_Array_t;
-     Index   : Natural) return String;
+    (Pointer : in Pointer_Array_t;
+     Index   : in Natural) return String;
 
   function Index_Terminated
-    (Pointer : Pointer_Array_t;
-     Index   : Natural) return Ada.Strings.Unbounded.Unbounded_String;
+    (Pointer : in Pointer_Array_t;
+     Index   : in Natural) return Ada.Strings.Unbounded.Unbounded_String;
+
+  function Index
+    (Pointer : in Allocated_Pointer_Array_t;
+     Size    : in Natural;
+     Index   : in Natural) return String;
+
+  function Index
+    (Pointer : in Allocated_Pointer_Array_t;
+     Size    : in Natural;
+     Index   : in Natural) return Ada.Strings.Unbounded.Unbounded_String;
+
+  function Index_Terminated
+    (Pointer : in Allocated_Pointer_Array_t;
+     Index   : in Natural) return String;
+
+  function Index_Terminated
+    (Pointer : in Allocated_Pointer_Array_t;
+     Index   : in Natural) return Ada.Strings.Unbounded.Unbounded_String;
 
   --
   -- for (Index = 0; Index < Size; ++Index)
@@ -44,7 +67,28 @@ package C_String.Arrays is
   --
 
   function Convert
-    (Pointer : Pointer_Array_t;
-     Size    : Natural) return String_Array_t;
+    (Pointer : in Pointer_Array_t;
+     Size    : in Natural) return String_Array_t;
+
+  function Convert
+    (Pointer : in Allocated_Pointer_Array_t;
+     Size    : in Natural) return String_Array_t;
+
+  --
+  -- Convert Ada -> C
+  --
+
+  function Convert_Terminated
+    (Strings : in String_Array_t) return Allocated_Pointer_Array_t;
+
+  function Convert
+    (Strings : in String_Array_t) return Allocated_Pointer_Array_t;
+
+  procedure Deallocate_Terminated
+    (Pointer : in Allocated_Pointer_Array_t);
+
+  procedure Deallocate
+    (Pointer : in Allocated_Pointer_Array_t;
+     Size    : in Natural);
 
 end C_String.Arrays;
